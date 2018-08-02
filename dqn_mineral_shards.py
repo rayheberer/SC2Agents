8793f47dfd2ef12c8fc59a81a72945caba386aa4
rayheberer <ray.heberer@gmail.com>
2018-08-02 10:44:33 -0700

    Agent queues random movements

--- dqn_mineral_shards.py
+++ dqn_mineral_shards.py
@@ -5,14 +5,19 @@ from pysc2.lib import actions, features
 
 FUNCTIONS = actions.FUNCTIONS
 
+# these are defaults set in pysc2.bin.agent
+feature_screen_size = 84
+feature_minimap_size = 64
+
+
 class DeepQMineralShards(base_agent.BaseAgent):
-	"""A deep Q-learning agent that can only select units and move"""
+    """A deep Q-learning agent that can only select units and move"""
 
-	def step(self, obs):
-		super(DeepQMineralShards, self).step(obs)
+    def step(self, obs):
+        super(DeepQMineralShards, self).step(obs)
 
-		if FUNCTIONS.Move_screen.id in obs.observation.available_actions:
-			print(obs.observation.feature_screen.player_relative)
-			return FUNCTIONS.no_op()
-		else:
-			return FUNCTIONS.select_army("select")
+        if FUNCTIONS.Move_screen.id in obs.observation.available_actions:
+            move_to = np.random.randint(0, feature_screen_size, 2)
+            return FUNCTIONS.Move_screen("queued", move_to)
+        else:
+            return FUNCTIONS.select_army("select")
