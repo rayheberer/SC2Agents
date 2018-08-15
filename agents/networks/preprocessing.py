3e231f4f4957b4db9ffc7d226c162244d2e9b448
rayheberer <ray.heberer@gmail.com>
2018-08-14 17:41:05 -0700

    Implement categorical embedding and log transform

--- agents/networks/preprocessing.py
+++ agents/networks/preprocessing.py
@@ -1,31 +1,65 @@
 """Tensorflow graphs that preprocess pysc2 features."""
 import tensorflow as tf
 
-from pysc2.lib import static_data
+from pysc2.lib import features as sc2_features
 
-UNIT_TYPES = max(static_data.UNIT_TYPES)
-# (channel index, number of categories)
-CATEGORICAL_SCREEN_FEATURES = [(1, 4),
-                               (2, 2),
-                               (3, 2),
-                               (4, 17),
-                               (5, 5),
-                               (6, UNIT_TYPES + 1),
-                               (7, 2),
-                               (16, 16)]
+#  get (channel index, categories) for categorical screen/minimap features
+SCREEN_FEATURES = [
+    (feature.type, feature.scale)
+    for feature in sc2_features.SCREEN_FEATURES
+]
 
-CATEGORICAL_MINIMAP_FEATURES = [(1, 4),
-                                (2, 2),
-                                (3, 2),
-                                (4, 17),
-                                (5, 5),
-                                (6, 2)]
+MINIMAP_FEATURES = [
+    (feature.type, feature.scale)
+    for feature in sc2_features.MINIMAP_FEATURES
+]
 
 
-def embed_screen_features(screen_features):
-    """Embed categorical screen features into continuous space."""
-    pass
+def preprocess_spatial_features(features, screen=True):
+    """Embed categorical spatial features, log transform numeric features."""
+    if screen:
+        feature_specs = SCREEN_FEATURES
+    else:
+        feature_specs = MINIMAP_FEATURES
 
+    # transpose from (channels, y, x) to (x, y, channels)
+    transposed = tf.transpose(
+        features,
+        perm=[2, 1, 0],
+        name='transpose')
 
-def embed_minimap_features(minimap_features):
-    """Embed categorical minimap features into continuous space."""
+    # expand dims so conv layers will work
+    expanded = tf.expand_dims(
+        transposed,
+        axis=0,
+        name='expand_dims')
+
+    preprocess_ops = []
+    for index, (feature_type, scale) in enumerate(feature_specs):
+        layer = expanded[:, :, :, index]
+
+        if feature_type == sc2_features.FeatureType.CATEGORICAL:
+            # one-hot encode in channel dimension -> 1x1 convolution
+            one_hot = tf.one_hot(
+                layer,
+                depth=scale,
+                axis=-1,
+                name='one_hot')
+
+            embed = tf.layers.conv2d(
+                inputs=one_hot,
+                filters=1,
+                kernel_size=[1, 1],
+                strides=[1, 1],
+                padding='SAME')
+
+            preprocess_ops.append(tf.squeeze(embed))
+        else:
+            transform = tf.log(
+                tf.cast(layer, tf.float32) + 1.,
+                name='log')
+
+            preprocess_ops.append(tf.squeeze(transform))
+
+    preprocessed = tf.stack(preprocess_ops, -1)
+    return preprocessed
